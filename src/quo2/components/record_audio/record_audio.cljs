a770ad78f809ce69325e9635c3d34c09f2951842
Brian Sztamfater <brian@status.im>
2022-10-07 14:39:51 -0300

    add 3 signal animation circles

--- src/quo2/components/record_audio/record_audio.cljs
+++ src/quo2/components/record_audio/record_audio.cljs
@@ -23,31 +23,63 @@
 (defn apply-anim [dd-height val]
   (reanimated/use-shared-value 1))
 
+(defn signal-circle [scale opacity]
+  (let [circle-style (reanimated/apply-animations-to-style
+                      {:transform [{:scale scale}]
+                       :opacity opacity}
+                      {:width  56
+                       :height 56
+                       :border-width 2
+                       :border-color colors/primary-50
+                       :border-radius 28
+                       :position :absolute
+                       :top 0
+                       :left 0})
+        scale-animation (reagent/atom nil)
+        opacity-animation (reagent/atom nil)]
+    [reanimated/view {:style circle-style}]))
+
+(defn signal-circle-apply-animations [scale opacity]
+  (reanimated/apply-animations-to-style
+   {:transform [{:scale scale}]
+    :opacity opacity}
+   {:width  56
+    :height 56
+    :border-width 1
+    :border-color colors/primary-50
+    :border-radius 28
+    :position :absolute
+    :top 0
+    :left 0}))
+
+(def scale-to 2)
+(def opacity-from 0.5)
+(def signal-anim-duration 1500)
+
 (defn record-button [on-record-start on-press-release]
   [:f>
    (fn []
      (let [recording? (reagent/atom false)
-           scale (reanimated/use-shared-value 1)
-           opacity (reanimated/use-shared-value 1)
+           scale-1 (reanimated/use-shared-value 1)
+           opacity-1 (reanimated/use-shared-value opacity-from)
+           scale-2 (reanimated/use-shared-value 1)
+           opacity-2 (reanimated/use-shared-value opacity-from)
+           scale-3 (reanimated/use-shared-value 1)
+           opacity-3 (reanimated/use-shared-value opacity-from)
           ;;  circle-animation (reanimated/use-animated-style )
-           circle-style (reanimated/apply-animations-to-style
-                         {:transform [{:scale scale}]
-                          :opacity opacity}
-                         {:width  56
-                          :height 56
-                          :border-width 2
-                          :border-color colors/primary-50
-                          :border-radius 28
-                          :position :absolute
-                          :top 0
-                          :left 0})
-           scale-animation (reagent/atom nil)
-           opacity-animation (reagent/atom nil)]
+           scale-animation-1 (reagent/atom nil)
+           opacity-animation-1 (reagent/atom nil)
+           scale-animation-2 (reagent/atom nil)
+           opacity-animation-2 (reagent/atom nil)
+           scale-animation-3 (reagent/atom nil)
+           opacity-animation-3 (reagent/atom nil)]
        [rn/view {:style {:width  56
                          :height 56}}
         ;; (when @recording?
           [:<>
-           [reanimated/view {:style circle-style}]]
+           [reanimated/view {:style (signal-circle-apply-animations scale-1 opacity-1)}]
+           [reanimated/view {:style (signal-circle-apply-animations scale-2 opacity-2)}]
+           [reanimated/view {:style (signal-circle-apply-animations scale-3 opacity-3)}]]
           ;; )
         [rn/touchable-opacity {:style {:position :absolute
                                        :top 0
@@ -55,49 +87,73 @@
                                        :width  56
                                        :height 56
                                        :background-color colors/primary-50
-                                       :border-radius 28}
-                               :active-opacity 0.8
+                                       :border-radius 28
+                                       :justify-content :center
+                                       :align-items :center}
+                               :active-opacity 1
                                :on-press-out #(do
-                                                (println "finish animation" @scale-animation @recording?)
+                                                (println "finish animation" @scale-animation-1 @recording?)
                                                 (reset! recording? false)
-                                                (reanimated/cancel-animation scale) ;;(.-value scale))
-                                                (reanimated/set-shared-value scale 1)
-                                                (reanimated/cancel-animation opacity)
-                                                (reanimated/set-shared-value opacity 1)) ;;(.-value opacity)))
+                                                (reanimated/cancel-animation scale-1)
+                                                (reanimated/set-shared-value scale-1 1)
+                                                (reanimated/cancel-animation opacity-1)
+                                                (reanimated/set-shared-value opacity-1 opacity-from)
+
+                                                (reanimated/cancel-animation scale-2)
+                                                (reanimated/set-shared-value scale-2 1)
+                                                (reanimated/cancel-animation opacity-2)
+                                                (reanimated/set-shared-value opacity-2 opacity-from)
+
+                                                (reanimated/cancel-animation scale-3)
+                                                (reanimated/set-shared-value scale-3 1)
+                                                (reanimated/cancel-animation opacity-3)
+                                                (reanimated/set-shared-value opacity-3 opacity-from)) 
                                :on-long-press #(do
                                                  (println "start animation", @recording?)
-                                                (reset! recording? true)
-                                                 (reset! scale-animation (reanimated/animate-shared-value-with-delay-repeat
-                                                                          scale
-                                                                          1.5
-                                                                          1000
-                                                                          :linear
-                                                                          0
-                                                                          -1))
-                                                 (reset! opacity-animation (reanimated/animate-shared-value-with-delay-repeat
-                                                                            opacity
-                                                                            0
-                                                                            1000
+                                                 (reset! recording? true)
+                                                 (reset! scale-animation-1 (reanimated/animate-shared-value-with-delay-repeat
+                                                                            scale-1
+                                                                            scale-to
+                                                                            signal-anim-duration
                                                                             :linear
                                                                             0
                                                                             -1))
-                                                ;;  (reanimated/animate-shared-value-with-delay-repeat
-                                                ;;   scale-animation
-                                                ;;   1.5
-                                                ;;   1000
-                                                ;;   :linear
-                                                ;;   0
-                                                ;;   -1)
-                                                ;;  (reanimated/animate-shared-value-with-delay-repeat
-                                                ;;   opacity-animation
-                                                ;;   0
-                                                ;;   1000
-                                                ;;   :linear
-                                                ;;   0
-                                                ;;   -1)
-                                                 )}
-         [icons/icon :main-icons/audio20 {:container-style {:margin-bottom 2}
-                                          :color           colors/white
+                                                 (reset! opacity-animation-1 (reanimated/animate-shared-value-with-delay-repeat
+                                                                              opacity-1
+                                                                              0
+                                                                              signal-anim-duration
+                                                                              :linear
+                                                                              0
+                                                                              -1))
+                                                 (reset! scale-animation-2 (reanimated/animate-shared-value-with-delay-repeat
+                                                                            scale-2
+                                                                            scale-to
+                                                                            signal-anim-duration
+                                                                            :linear
+                                                                            500
+                                                                            -1))
+                                                 (reset! opacity-animation-2 (reanimated/animate-shared-value-with-delay-repeat
+                                                                              opacity-2
+                                                                              0
+                                                                              signal-anim-duration
+                                                                              :linear
+                                                                              500
+                                                                              -1))
+                                                 (reset! scale-animation-3 (reanimated/animate-shared-value-with-delay-repeat
+                                                                            scale-3
+                                                                            scale-to
+                                                                            signal-anim-duration
+                                                                            :linear
+                                                                            1000
+                                                                            -1))
+                                                 (reset! opacity-animation-3 (reanimated/animate-shared-value-with-delay-repeat
+                                                                              opacity-3
+                                                                              0
+                                                                              signal-anim-duration
+                                                                              :linear
+                                                                              1000
+                                                                              -1)))}
+         [icons/icon :main-icons/audio20 {:color           colors/white
                                           :size            20}]]]))])
 
 (defn send-button []
