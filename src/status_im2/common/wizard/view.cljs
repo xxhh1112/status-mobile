76fee7a39d08a2fb3689e41b4da202011c54fdbc
Ulises M <ulises.ssb@hotmail.com>
2023-05-29 15:51:39 +0100

    WIP 2

--- src/status_im2/common/wizard/view.cljs
+++ src/status_im2/common/wizard/view.cljs
@@ -4,7 +4,7 @@
             [react-native.reanimated :as reanimated]
             [reagent.core :as reagent]))
 
-(def slide-away-ms 5)
+(def slide-away-ms 100)
 (def slide-up-ms 3)
 
 (def slide-to-initial-ms 3)
@@ -13,135 +13,106 @@
 
 (def slide-to-center 10)
 
-(defn next-step-animation [translateX translateY opacity window-width window-height]
+(defn next-step-animation [translate-x translate-x-2 translate-x-3 window-width]
   (reanimated/set-shared-value
-   translateX
-   (reanimated/with-sequence
-     (reanimated/with-timing
-      (- window-width)
-       {:duration slide-away-ms
-        :easing (:easing1 reanimated/easings)})
-
-
-     (reanimated/with-timing window-width
-       {:duration slide-to-initial-ms
-        :easing (:easing1 reanimated/easings)})
-
-     (reanimated/with-timing 0
-       {:duration slide-to-center
-        :easing (:easing1 reanimated/easings)})))
+   translate-x-3
+   (reanimated/with-timing
+    (* (- window-width) 2)
+    {:duration slide-away-ms
+     :easing   (:easing1 reanimated/easings)}))
 
   (reanimated/set-shared-value
-   opacity
-   (reanimated/with-sequence
-     (reanimated/with-timing  0
-       {:duration 10
-        :easing (:easing1 reanimated/easings)})
-
-   (reanimated/with-delay 30 #_swap-content-ms
-     (reanimated/with-timing  1
-       {:duration 10
-        :easing (:easing1 reanimated/easings)}))))
-
-  #_(reanimated/set-shared-value
-   translateY
-   (reanimated/with-sequence
-     (reanimated/with-delay (* slide-away-ms 6)
-       (reanimated/with-timing window-height
-         {:duration slide-up-ms
-          :easing (:easing1 reanimated/easings)}))
-
-     (reanimated/with-timing 0
-       {:duration 10
-        :easing (:easing1 reanimated/easings)}))))
-
-(defn prev-step-animation [shared-val opacity window-width]
-  (reanimated/set-shared-value
-   shared-val
-   (reanimated/with-sequence
-     (reanimated/with-timing  (+ (* 2   window-width))
-       {:duration 25
-        :easing (:easing1 reanimated/easings)})
-
-     (reanimated/with-timing  (- (* 2 window-width))
-       {:duration 5
-        :easing (:easing1 reanimated/easings)})
-
-     (reanimated/with-timing  0
-       {:duration 25
-        :easing (:easing1 reanimated/easings)})))
+   translate-x
+   (reanimated/with-timing
+    (- window-width)
+    {:duration slide-away-ms
+     :easing   (:easing1 reanimated/easings)}))
 
   (reanimated/set-shared-value
-   opacity
-   (reanimated/with-sequence
-    (reanimated/with-timing
-     0
-     {:duration 25
-      :easing   (:easing1 reanimated/easings)})
-
-    (reanimated/with-delay
-     150
-     (reanimated/with-timing
-      1
-      {:duration 200
-       :easing   (:easing1 reanimated/easings)})))))
+   translate-x-2
+   (reanimated/with-timing
+    (- window-width)
+    {:duration slide-away-ms
+     :easing   (:easing1 reanimated/easings)})))
 
 (defn f-step
-  [{:keys [content animation duration translateX translateY opacity current-step
-           prev-step use-native-driver content-container-style]}]
+  [{:keys [content-container-style current-step translateX content]}]
   [reanimated/view {:style (reanimated/apply-animations-to-style
-                            {:transform [{:translateX translateX}
-                                         {:translateY translateY}]
-                             :opacity   opacity}
-                            {:position :absolute
-                             :top      0
-                             :bottom   0
-                             :left     0
-                             :right    0})}
+                            {:transform [{:translateX translateX}]}
+                            content-container-style)}
    content])
 
-(def page-container
+
+(defn page-container [window-width]
   {:position :absolute
    :top      0
    :bottom   0
-   :left     0
-   :right    0})
+   :left     window-width
+   :right    (- window-width)})
 
 (defn f-view
   [{:keys [steps active-step current-step on-next on-prev first-step? last-step? duration
            wizard-ref]}]
-  (let [active-step-no (reagent/atom 0)
-        prev-step-no   (reagent/atom 0)
+  (let [active-step-no    (atom 0)
+        following-step-no (atom 1)
+        previous-step-no  (atom -1) ;; TODO: consider we can "jump" steps
         {window-width  :width
          window-height :height} (rn/get-window)
-        next?          (reagent/atom true)]
+        next?             (reagent/atom true)]
     (fn []
-      (let [translateX (reanimated/use-shared-value 0)
-            translateY (reanimated/use-shared-value 0)
-            opacity    (reanimated/use-shared-value 1)]
+      (let [translateX    (reanimated/use-shared-value 0)
+            translateY    (reanimated/use-shared-value 0)
+            opacity       (reanimated/use-shared-value 1)
+            translate-x-2 (reanimated/use-shared-value 0)
+            translate-x-3 (reanimated/use-shared-value 0)
+            opacity-2     (reanimated/use-shared-value 1)]
         (oops/oset! wizard-ref "current"
                     {:next (fn []
+                             (prn "next step called")
                              (when (not= (- (count steps) 1) @active-step-no)
-                               (next-step-animation translateX translateY opacity window-width window-height)
-                               (js/setTimeout (fn []
-                                                (swap! active-step-no inc)
-                                                (reset! next? true))
-                                              swap-content-ms)))
+                               (next-step-animation translateX translate-x-2 translate-x-3 window-width)
+                               (swap! active-step-no inc)
+                               (swap! following-step-no inc)
+                               (reset! next? true)))
 
                      :prev (fn []
                              (when-not (zero? @active-step-no)
-                               (prev-step-animation translateX opacity window-width)
-                               (js/setTimeout (fn []
-                                                (swap! active-step-no dec)
-                                                (reset! next? true))
-                                              50)))})
+                               #_(prev-step-animation translateX opacity window-width)
+                               (swap! active-step-no dec)
+                               (swap! following-step-no dec)
+                               (reset! next? true)))})
+        #_[:<>
+         (when-not (zero? @active-step-no)
+           [:f> f-step {:content-container-style (page-container 0)
+                        :current-step            @active-step-no
+                        :translateX              translate-x-3
+                        :content                 (:content (nth steps (dec @active-step-no)))}])
 
-        [:f> f-step {:content-container-style page-container
-                     :current-step            @active-step-no
-                     :translateX              translateX
-                     :translateY              translateY
-                     :opacity                 opacity
-                     :content                 (:content (nth steps @active-step-no))}]))))
+         [:f> f-step {:content-container-style (page-container 0)
+                      :current-step            @active-step-no
+                      :translateX              translateX
+                      :content                 (:content (nth steps @active-step-no))}]
+
+         [:f> f-step {:content-container-style (page-container window-width)
+                      :current-step            @active-step-no
+                      :translateX              translate-x-2
+                      :content                 (:content (nth steps (inc @active-step-no)))}]]
+
+
+        (doall
+         (map-indexed (fn [step idx]
+                        (if (or (= idx (dec @active-step-no))
+                                (= idx @active-step-no)
+                                (= idx (inc @active-step-no)))
+                         [:f> f-step {:content-container-style (page-container 0)
+                                      :current-step            @active-step-no
+                                      :translateX              translateX
+                                      :content                 (:content (nth steps @active-step-no))}]))
+
+                      steps))
+
+
+        ))))
 
 (defn view [props]
   [:f> f-view props])
