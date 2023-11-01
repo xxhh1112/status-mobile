37b139fe9affb1f96d3d886a01407894dbb15cbd
Siddarth Kumar <siddarthkay@gmail.com>
2023-11-01 09:14:16 +0530

    lint the lint files

--- .clj-kondo/taoensso/encore/taoensso/encore.clj
+++ .clj-kondo/taoensso/encore/taoensso/encore.clj
@@ -1,16 +1,17 @@
 (ns taoensso.encore
   (:require
-   [clj-kondo.hooks-api :as hooks]))
+    [clj-kondo.hooks-api :as hooks]))
 
-(defn defalias [{:keys [node]}]
+(defn defalias
+  [{:keys [node]}]
   (let [[sym-raw src-raw] (rest (:children node))
-        src (if src-raw src-raw sym-raw)
-        sym (if src-raw
-              sym-raw
-              (symbol (name (hooks/sexpr src))))]
+        src               (if src-raw src-raw sym-raw)
+        sym               (if src-raw
+                            sym-raw
+                            (symbol (name (hooks/sexpr src))))]
     {:node (with-meta
              (hooks/list-node
-               [(hooks/token-node 'def)
-                (hooks/token-node (hooks/sexpr sym))
-                (hooks/token-node (hooks/sexpr src))])
+              [(hooks/token-node 'def)
+               (hooks/token-node (hooks/sexpr sym))
+               (hooks/token-node (hooks/sexpr src))])
              (meta src))}))
